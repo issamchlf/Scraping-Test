5ac2815091b1063ceeeb40aca010cf4906575dc7
issamchlf <issamchellaf734@gmail.com>
2025-04-25 15:07:53 +0200

    Feat : Add Event model with fillable attributes and update routes for event scraping

---
+++ app/Models/Event.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+    protected $fillable = [
+        'title',
+        'date',
+        'description',
+        'image_url',
+        'link',
+    ];
+}
--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ScrapingController;
+use App\Http\Controllers\EsMadridScraperController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/scrape', [ScrapingController::class, 'scrape']);
+Route::get('/scrape-events', [EsMadridScraperController::class, 'scrape']);
+Route::get('/events', [EsMadridScraperController::class, 'index'])->name('events.index');
